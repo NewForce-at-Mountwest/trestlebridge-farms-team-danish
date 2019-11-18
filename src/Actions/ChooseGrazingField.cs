7cc47b7a61148242207a2994fed6e40975f3dd69
lmsamples <lmarcuss2000@gmail.com>
2019-11-18 14:23:46 -0500

    user can add graizing animals

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -11,7 +11,17 @@ namespace Trestlebridge.Actions
         // Collecting the added Grazing Fields
         public static void CollectInput(Farm farm, IGrazing animal)
         {
+
             Utils.Clear();
+            Console.WriteLine();
+            Console.WriteLine(@"
+        +-++-++-++-++-++-++-++-++-++-++-++-++-+
+        |T||r||e||s||t||l||e||b||r||i||d||g||e|
+        +-++-++-++-++-++-++-++-++-++-++-++-++-+
+                    |F||a||r||m||s|
+                    +-++-++-++-++-+");
+            Console.WriteLine();
+
             // Loop throughh created Facilities and list them by number
             for (int i = 0; i < farm.GrazingFields.Count; i++)
             {
@@ -27,7 +37,13 @@ namespace Trestlebridge.Actions
             // User Inputted number
             int choice = Int32.Parse(Console.ReadLine());
             // Using the Inputted Number in association with the Facility and adding animal
-            farm.GrazingFields[choice].AddResource(animal);
+            if(choice > 0 && choice < 21){
+                farm.GrazingFields[choice-1].AddResource(animal);
+            } else {
+                Console.WriteLine();
+                Console.WriteLine("Value not recognized, press return to exit.");
+                Console.ReadLine();
+            }
 
             /*
                 Couldn't get this to work. Can you?
--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,22 +4,28 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
 
 
-        public static void CollectInput (Farm farm) {
+        public static void CollectInput(Farm farm)
+        {
 
             //farm refers to every grazing field
 
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Pig");
+            Console.WriteLine("3. Goat");
+            Console.WriteLine("4. Ostrich");
+            Console.WriteLine("5. Sheep");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             //set a string to the number typed in
 
@@ -30,9 +36,47 @@ namespace Trestlebridge.Actions {
             {
                 case 1:
 
-                // if the user types in 1 run this
+                    // if the user types in 1 run this
 
                     ChooseGrazingField.CollectInput(farm, new Cow());
+
+                    // from here the application knows you would like to purchase a cow and will then ask where to put it in ChooseGrazingField
+
+                    break;
+
+                case 2:
+
+                    // if the user types in 1 run this
+
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+
+                    // from here the application knows you would like to purchase a cow and will then ask where to put it in ChooseGrazingField
+
+                    break;
+
+                case 3:
+
+                    // if the user types in 1 run this
+
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+
+                    // from here the application knows you would like to purchase a cow and will then ask where to put it in ChooseGrazingField
+
+                    break;
+                case 4:
+
+                    // if the user types in 1 run this
+
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+
+                    // from here the application knows you would like to purchase a cow and will then ask where to put it in ChooseGrazingField
+
+                    break;
+                case 5:
+
+                    // if the user types in 1 run this
+
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
 
                     // from here the application knows you would like to purchase a cow and will then ask where to put it in ChooseGrazingField
 
